340c33ca4b85261824a996c521edd9c3dbc6b849
Polin Liao <prob1995@gmail.com>
2015-04-06 03:20:25 +0000

    Copy herodj

--- djtest/wsgi.py
+++ djtest/wsgi.py
@@ -13,7 +13,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djtest.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
+#FOR HEROKU DEPLOY
+'''
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
+'''
